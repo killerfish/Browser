dcc86eb14ac993af1d18d0252dcf9692587ab83c
Usman Raza <usman.r123@gmail.com>
2023-04-12 13:29:25 +0500

    This commit updates the address bar ---------------------------------------------------
    - Removed the input accessory view
    - Now the single urlTextfield is animated with keyboard

--- Orion/WebViewController.swift
+++ Orion/WebViewController.swift
@@ -11,18 +11,20 @@ import Zip
 
 class WebViewController: UIViewController {
   private var webView: WKWebView!
-  private var urlField: UITextField!
-  private var urlInputAccessoryView: URLInputAccessoryView!
   private var addressBar: UIView!
-  private var ignoreKeyboardNotification = false
+  private var urlField: UITextField!
+  private var cancelButton: UIButton!
+  private var viewAnimator: UIViewPropertyAnimator?
+  private var urlFieldWidthConstraint: NSLayoutConstraint!
+  private var isKeyboardShowing = false
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     makeWebView()
     makeAddressBar()
-    makeURLInputAccessoryView()
     makeURLField()
+    makeCancelButton()
     setupNotifications()
     setupConstraints()
     
@@ -47,23 +49,26 @@ class WebViewController: UIViewController {
     view.addSubview(addressBar)
   }
   
-  func makeURLInputAccessoryView() {
-    urlInputAccessoryView = URLInputAccessoryView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 66))
-    urlInputAccessoryView.cancelButton.addTarget(self, action: #selector(cancelButtonPressed), for: .touchUpInside)
-    urlInputAccessoryView.urlTextField.delegate = self
-  }
-  
   func makeURLField() {
     urlField = UITextField()
     urlField.translatesAutoresizingMaskIntoConstraints = false
     urlField.delegate = self
     urlField.setAsURLTextField()
-    urlField.inputAccessoryView = urlInputAccessoryView
     addressBar.addSubview(urlField)
   }
   
+  func makeCancelButton() {
+    cancelButton = UIButton(type: .system)
+    cancelButton.translatesAutoresizingMaskIntoConstraints = false
+    cancelButton.setTitle("Cancel", for: .normal)
+    cancelButton.addTarget(self, action: #selector(cancelButtonPressed), for: .touchUpInside)
+    addressBar.addSubview(cancelButton)
+    cancelButton.isHidden = true
+  }
+  
   func setupNotifications() {
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
   }
   
   func setupConstraints() {
@@ -79,11 +84,17 @@ class WebViewController: UIViewController {
       addressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       
       urlField.heightAnchor.constraint(equalToConstant: 44),
-      urlField.widthAnchor.constraint(equalTo: addressBar.widthAnchor, multiplier: 0.85),
       urlField.centerYAnchor.constraint(equalTo: addressBar.centerYAnchor),
-      urlField.centerXAnchor.constraint(equalTo: addressBar.centerXAnchor)
+      urlField.leftAnchor.constraint(equalTo: addressBar.leftAnchor, constant: 16),
+      
+      cancelButton.widthAnchor.constraint(equalToConstant: 66),
+      cancelButton.heightAnchor.constraint(equalToConstant: 44),
+      cancelButton.centerYAnchor.constraint(equalTo: addressBar.centerYAnchor),
+      cancelButton.rightAnchor.constraint(equalTo: addressBar.rightAnchor, constant: -16)
     ]
     
+    urlFieldWidthConstraint = urlField.widthAnchor.constraint(equalTo: addressBar.widthAnchor, constant: -32)
+    urlFieldWidthConstraint.isActive = true
     NSLayoutConstraint.activate(constraints)
   }
 }
@@ -140,7 +151,7 @@ extension WebViewController: WKNavigationDelegate {
 
 extension WebViewController: UITextFieldDelegate {
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    if textField == urlInputAccessoryView.urlTextField, let text = textField.text {
+    if let text = textField.text {
       urlField.text = text
       
       if let url = URL(string: text), url.isValid() {
@@ -157,10 +168,7 @@ extension WebViewController: UITextFieldDelegate {
       }
     }
     
-    urlInputAccessoryView.urlTextField.resignFirstResponder()
     urlField.resignFirstResponder()
-    ignoreKeyboardNotification = false
-    
     return true
   }
 }
@@ -176,17 +184,53 @@ extension WebViewController {
     webView.goBack()
   }
   
-  @objc
-  func keyboardDidShow(_ notification: Notification) {
-    if ignoreKeyboardNotification == false {
-      urlInputAccessoryView.urlTextField.becomeFirstResponder()
-      ignoreKeyboardNotification = true
+  @objc func keyboardWillShow(_ notification: Notification) {
+    guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+    
+    if isKeyboardShowing == false {
+      isKeyboardShowing = true
+    } else {
+      return
     }
+    
+    let keyboardTopY = keyboardFrame.minY
+    let addressBarBottomY = addressBar.convert(addressBar.bounds, to: view).maxY
+    let offsetY = addressBarBottomY - keyboardTopY
+
+    viewAnimator?.stopAnimation(true)
+    viewAnimator = UIViewPropertyAnimator(duration: 0.32, curve: .linear, animations: {
+      self.view.frame.origin.y = -offsetY
+      self.urlFieldWidthConstraint.constant -= 75
+      self.view.layoutIfNeeded()
+    })
+    
+    viewAnimator?.addCompletion({ position in
+      self.cancelButton.isHidden = false
+    })
+    
+    viewAnimator?.startAnimation()
   }
   
   @objc
+  func keyboardWillHide(_ notification: Notification) {
+    viewAnimator?.stopAnimation(true)
+    viewAnimator = UIViewPropertyAnimator(duration: 0.1, curve: .linear, animations: {
+      self.view.frame.origin.y = 0
+      self.urlFieldWidthConstraint.constant = -32
+      self.view.layoutIfNeeded()
+    })
+    
+    viewAnimator?.addCompletion({ position in
+      self.cancelButton.isHidden = true
+    })
+    
+    viewAnimator?.startAnimation()
+    isKeyboardShowing = false
+  }
+
+  
+  @objc
   func cancelButtonPressed() {
-    urlInputAccessoryView.urlTextField.resignFirstResponder()
     urlField.resignFirstResponder()
   }
 }
